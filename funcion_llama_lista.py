06df3da2e1638e106ae1f787aff2013793410de6
ceblanc <71959348+ceblanc@users.noreply.github.com>
2020-10-07 19:00:38 +0200

    Add files via upload

--- funcion_llama_lista.py
+++ funcion_llama_lista.py
@@ -1,22 +1,45 @@
+margenes=[1.45,1.52,1.68]
+
+area=int(input("Selecciona un área:\n""\n"
+               "1.Supermercado: \n"
+               "2.Herbolario: \n"
+               "3.Frutería: \n""\n"
+               "Selecciona 0 para salir del programa""\n""\n"
+               "Selección: "))
+
 def crea_lista():
 
     datos=[]
 
-    coste=float(input("Coste: "))
-    margen=float(input("Margen: "))
-    iva=float(input("Iva: "))
+    coste=float(input("\nPor favor, introduce un coste: "))
+    iva=float(input("Por favor, introduce un IVA: "))
 
     datos.append(coste)
-    datos.append(margen)
     datos.append(iva)
 
     return datos
 
 def operacion(datos):
 
-    resultado=datos[0]*datos[1]+(datos[0]*datos[2]/100)
+    if area==1:
+
+        resultado=datos[0]*margenes[0]+(datos[0]*datos[1]/100)
         
-    return resultado
+        return resultado
+
+    elif area==2:
+
+        resultado=datos[0]*margenes[1]+(datos[0]*datos[1]/100)
+        
+        return resultado
+    
+    elif area==3:
+
+        resultado=datos[0]*margenes[2]+(datos[0]*datos[1]/100)
+        
+        return resultado
 
 precio=crea_lista()
-print(operacion(precio))
+
+print("El precio en punto de venta es: ",(round(operacion(precio),2)))
+print("El precio en punto de venta es: ",(round(operacion(precio)*1.04,2)))
